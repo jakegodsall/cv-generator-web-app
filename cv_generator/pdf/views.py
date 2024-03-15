4ca1f19242d8266446b1d94d1ad2e65b12e6ad47
jakegodsall <jake.edward.godsall@gmail.com>
2024-03-15 13:44:19 +0100

    added ModelForm for Profile model, and basic view function and template rendering

--- cv_generator/pdf/views.py
+++ cv_generator/pdf/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .forms import ProfileForm
 
 # Create your views here.
+def index(request):
+    if request.method == "POST":
+        ...
+    form = ProfileForm()
+    return render(request, "pdf/index.html", {"form": form})
